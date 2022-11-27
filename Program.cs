a96c405896af38e40128f74f4f872297e03bb4ce
Matheus Alexandre <matheuscurses@gmail.com>
2022-11-26 22:47:26 -0300

    Enumeracoes e Composicao - aula 124

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CapEnum.Entities;
+using CapEnum.Entities.Enums;
+
+namespace CapEnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine(txt); // converter um valor enum para string
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+
+        }
+    }
+}
