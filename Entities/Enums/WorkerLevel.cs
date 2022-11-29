a202d286b1d575d46564b48886c4e37ef66b06ee
Matheus Alexandre <matheuscurses@gmail.com>
2022-11-28 22:15:35 -0300

    Corrigido

---
+++ Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,11 @@
+﻿
+namespace CapEnum.Entities.Enums
+{
+    enum WorkerLevel : int
+    { 
+        Junior = 0,
+        Pleno = 1,
+        Senior = 2
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using CapEnum.Entities;
 using CapEnum.Entities.Enums;
+using System.Globalization;
 
 namespace CapEnum
 {
@@ -8,20 +9,56 @@ namespace CapEnum
     {
         static void Main(string[] args)
         {
-            Order order = new Order
+            try
             {
-                Id = 1080,
-                Moment = DateTime.Now,
-                Status = OrderStatus.PendingPayment
-            };
+                Console.Write("Enter department´s name: ");
+                string deptName = Console.ReadLine();
+                Console.WriteLine("Enter worker data:");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Level (Junior/Pleno/Senior): ");
+                WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
+                Console.Write("Base Salary: ");
+                double baseSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            Console.WriteLine(order);
+                Department dept = new Department(deptName);
+                Worker worker = new Worker(name, level, baseSalary, dept);
 
-            string txt = OrderStatus.PendingPayment.ToString();
-            Console.WriteLine(txt); // converter um valor enum para string
+                Console.Write("How many contracts to this worker?: ");
+                int n = int.Parse(Console.ReadLine());
+                for (int i = 1; i <= n; i++)
+                {
+                    Console.WriteLine($"Enter #{i} contract data:");
+                    Console.Write("Date (DD/MM/YYYY): ");
+                    DateTime date = DateTime.Parse(Console.ReadLine());
+                    Console.Write("Value Per Hour: ");
+                    double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.Write("Duration: ");
+                    int duration = int.Parse(Console.ReadLine());
+                    HourContract contract = new HourContract(date, valuePerHour, duration);   
+                    worker.AddContract(contract);
+                }
 
-            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
-            Console.WriteLine(os);
+                Console.WriteLine();
+                Console.Write("Enter month and year to calculate income (MM/YYYY): ");
+                string mountAndYear = Console.ReadLine();
+
+                int month = int.Parse(mountAndYear.Substring(0, 2));
+                int year = int.Parse(mountAndYear.Substring(3));
+
+                Console.WriteLine("Name " + worker.Name);
+                Console.WriteLine("Department " + worker.Department.Name);
+                Console.WriteLine("Income for " + mountAndYear + ": " + worker.Income(year, month));
+
+
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+
 
         }
     }
