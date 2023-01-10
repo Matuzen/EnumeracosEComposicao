3a6fd57bbdbf688f7fc0e4689c41d3e6522216e7
Matheus Alexandre <matheuscurses@gmail.com>
2023-01-10 17:38:34 -0300

    Adicionar arquivos de projeto.

---
+++ Entities/Order.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Enumeracoes.Entities.Enums;
+
+namespace Enumeracoes.Entities
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public Order()
+        {
+        }
+
+        public Order(int id, DateTime moment, OrderStatus status)
+        {
+            Id = id;
+            Moment = moment;
+            Status = status;
+        }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Moment
+                + ", "
+                + Status;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Enumeracoes.Entities;
+using Enumeracoes.Entities.Enums;
+
+namespace Enumeracoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order(1080, DateTime.Now, OrderStatus.PendingPayment);
+            Console.WriteLine(order);
+
+            Order order1 = new Order 
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+            Console.WriteLine(order1);
+
+            // Conversão de string para enumeração, valor enumerado para string
+
+            string txt = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine(txt);
+
+            // Conversão de enum para string
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+
+
+        }
+    }
+}
